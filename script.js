cb75b68a6b6e77cab0f2bc0e2380bbbda1e874d5
Isaac <isaackennedyfds@gmail.com>
2018-11-30 13:51:46 -0200

    Melhoria da interface

--- script.js
+++ script.js
@@ -14,30 +14,36 @@ var palavras = [
   ['mo','chi','la'],
   ['pa','ra','le','le','pí','pe','do'],
   ['in','cons','ti','tu','cio','na','lis','ci','ma','men','te'],
-  ['pneu','mo','ul','tra','mi','cros','co','pi','co','si','li','co','vul','ca','no','co','ni','ó','ti','co']
+  ['pneu','mo','ul','tra','mi','cros','co','pi','cos','si','li','co','vul','ca','no','co','ni','ó','ti','co']
 ];
 
 var pontuacao = 0;
+var vidas = 3;
 var indexAtual;
 var palavraAtual;
 var palavrasTentadas = new Array();
 var tentativas = 0;
 
 function geraPalavra() {
-  if (palavrasTentadas.length < palavras.length) {
-    let index;
-    do {
-      index = Math.floor(Math.random()*palavras.length);
-    } while (palavrasTentadas.includes(index));
+  if (vidas > 0) {
+	  if (palavrasTentadas.length < palavras.length || palavrasTentadas.length <= 10) {
+		let index;
+		do {
+		  index = Math.floor(Math.random()*palavras.length);
+		} while (palavrasTentadas.includes(index));
 
-    indexAtual = index;
-    palavraAtual = palavras[indexAtual];
-    palavrasTentadas.push(indexAtual);
+		indexAtual = index;
+		palavraAtual = palavras[indexAtual];
+		palavrasTentadas.push(indexAtual);
+	  } else {
+		alert("Parabéns! Você acertou " + pontuacao + " de " + palavras.length + " palavras.");
+		location.href="index.html";
+	  }
   } else {
-    alert("Parabéns! Você acertou " + pontuacao + " de " + palavras.length + " palavras.");
-    location.href="index.html";
+	  alert("Você perdeu :( mas não desanime, tente novamente!");
+	  location.href="index.html";
   }
-  $("#palavra").html(palavraAtual);
+	  $("#palavra").html(palavraAtual);
 }
 
 function processarResposta() {
@@ -58,9 +64,11 @@ $("#responder").on("click", function() {
     pontuacao++;
     alert("Resposta correta!");
   } else {
+    vidas--;
     alert("Resposta errada :(");
   }
-
+  
+  $("#vidas").html(vidas);
   $("#pontuacao").html(pontuacao +"/"+(tentativas));
 
   $("#entrada").val("");
